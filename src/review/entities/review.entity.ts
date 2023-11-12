edf8b93adca0b45cecbd5fba8886ae9a127df905
雪小花 <lijiaxin@mtfxh.wiki>
2023-11-12 11:20:32 +0800

    编写用户创建文章

---
+++ src/review/entities/review.entity.ts
@@ -0,0 +1,21 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { User } from '../../user/entities/user.entity';
+
+@Entity()
+export class Review {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column({ type: 'longtext', nullable: true })
+  commentContent: string;
+  @ManyToOne(() => User, (user) => user.reviews)
+  @JoinColumn()
+  user: number;
+  @Column({ type: 'bigint', default: () => new Date().getTime() })
+  createdTime: number;
+}
