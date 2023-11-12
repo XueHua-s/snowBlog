0fed3766002b9141d6fbf6ff2f60d2f0588aab7c
雪小花 <lijiaxin@mtfxh.wiki>
2023-11-12 14:07:19 +0800

    优化用户逻辑

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -53,7 +53,7 @@ export class UserService {
   // 更新用户关联信息(传user实体)
   async updateUserInfo(proFile: UserProfileDto) {
     if (!/^(http|https):\/\//.test(proFile.homepage)) {
-      throw new HttpException('主页不是一个http / https', 202)
+      throw new HttpException('主页不是一个http或https', 202);
     }
     // 获取用户信息实例
     const proFileDetail = await this.getUserInfoById(proFile.user.id);
