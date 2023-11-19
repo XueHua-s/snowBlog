a54a057a22bef42516aeb4fe15f625272b616be9
雪小花 <lijiaxin@mtfxh.wiki>
2023-11-19 14:31:58 +0800

    增加了ui框架全局导入

--- blog/src/main.ts
+++ blog/src/main.ts
@@ -5,10 +5,11 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+import Layui from '@layui/layui-vue'
+import '@layui/layui-vue/lib/index.css'
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(Layui)
 app.mount('#app')
