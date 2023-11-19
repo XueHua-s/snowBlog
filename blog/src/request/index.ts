ff870f39514b9fd4346da6fba99b13586f30daee
雪小花 <lijiaxin@mtfxh.wiki>
2023-11-19 14:42:37 +0800

    优化axios导入

--- blog/src/request/index.ts
+++ blog/src/request/index.ts
@@ -1,16 +1,15 @@
-import axios, {AxiosResponse} from 'axios';
-import { AxiosRequestConfig, AxiosError } from 'axios'
+import axios, {AxiosResponse, InternalAxiosRequestConfig, AxiosError} from 'axios';
 const request = axios.create({
   timeout: 180000
 })
 // 请求拦截
-request.interceptors.request((config: AxiosRequestConfig) => {
+request.interceptors.request.use((config: InternalAxiosRequestConfig) => {
   return config
 }, (err: AxiosError) => {
   return err
 })
 // 响应拦截
-request.interceptors.response((res: AxiosResponse) => {
+request.interceptors.response.use((res: AxiosResponse) => {
   return res
 }, (err: AxiosError) => {
   return err
